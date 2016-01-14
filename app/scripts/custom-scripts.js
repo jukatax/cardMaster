4ce840779787796e4dc49f239b8d488046f01dc1
Yuliyan Yordanov <YuliyanYordanov@powa.com>
2016-01-14 17:04:42 +0000

    adding the angular functionality and html to the site, starterd working on the default theme,changed the loaction for bootstrap fonts.

--- app/scripts/custom-scripts.js
+++ app/scripts/custom-scripts.js
@@ -1,4 +1,29 @@
 /**
  * Created by b12mac on 10/12/15.
  */
-jQuery('h1').css({'font-size':'4em'});
+//jQuery('h1').css({'font-size':'4em'});
+/*####################################################*/
+/*#################### ng-app ########################*/
+/*####################################################*/
+var app = angular.module('cardMaster', []);
+app.controller('cardMaster' , function($scope){
+
+    //show hide comma if both values are present for city and postcode
+    $('#pc,#city').on('blur',function(){
+        if($('#city').val()!='' && $('#pc').val()!=''){
+            $('.comma').show();
+        }else{
+            $('.comma').hide();
+        }
+    });
+
+});
+app.directive('cardPreview' , function(){
+    return {
+        restrict: 'EAC',
+        template: ''
+    }
+});
+/*####################################################*/
+/*################# END ng-app #######################*/
+/*####################################################*/
